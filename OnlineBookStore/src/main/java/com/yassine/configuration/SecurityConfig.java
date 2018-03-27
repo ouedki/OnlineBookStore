33946f23f3f513b8b0259e9e53c2e53a467ce3c6
yassine <yassine@192.168.1.4>
2018-03-27 13:28:09 +0100

    added config class and dependencies to use Redis session

--- OnlineBookStore/src/main/java/com/yassine/configuration/SecurityConfig.java
+++ OnlineBookStore/src/main/java/com/yassine/configuration/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.yassine.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.session.web.http.HttpSessionStrategy;
 
 import com.yassine.services.UserSecurityService;
 
@@ -42,5 +45,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.csrf().disable().cors().disable().httpBasic()
 		.and().authorizeRequests().antMatchers(PUBLIC_MATCHERS)
 		.permitAll().anyRequest().authenticated();
+	}
+	
+	@Bean
+	public HttpSessionStrategy sessionStrategy() {
+		return new HeaderHttpSessionStrategy();
 	}
 }
